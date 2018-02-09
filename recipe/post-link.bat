c14f38a88d3fa6a95e9a894eb05cb48571f71e61
Landung Setiawan <landungs@uw.edu>
2018-02-09 12:11:10 -0800

    Update post-link.bat

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable nbserverproxy --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-serverextension.exe" enable nbserverproxy --py --sys-prefix && if errorlevel 1 exit 1
