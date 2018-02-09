af40639383a6d20799e7b7e5396d594035f9677c
Landung Setiawan <landungs@uw.edu>
2018-02-09 12:11:40 -0800

    Update pre-unlink.bat

--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" disable nbserverproxy --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-serverextension.exe" disable nbserverproxy --py --sys-prefix && if errorlevel 1 exit 1
